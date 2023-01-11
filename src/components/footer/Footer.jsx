91c6022c3bb900f0ec919b76bfb84fda59f04dfe
AlexBrown4020 <balex3497@gmail.com>
2023-01-11 13:03:51 +0900

    Feat: modify layout of footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,10 +4,14 @@ export const Footer = () => {
 
     return (
         <footer className='footer'>
-            <p className='creatorDetails'>Created by: Alex Brown</p>
-            <a href='https://github.com/AlexBrown4020/mern-project' className='creatorDetails'>Github</a>
-            <a href='https://www.linkedin.com/in/alex-brown-2a6b7820a/' className='creatorDetails'>LinkedIn</a>
-            <p className='creatorDetails'>Last Updated: 10th Jan 2023</p>
+            <div className='footerDetails'>
+                <a href='https://github.com/AlexBrown4020/mern-project' className='creatorDetails'>Github</a>
+                <a href='https://www.linkedin.com/in/alex-brown-2a6b7820a/' className='creatorDetails'>LinkedIn</a>
+            </div>
+            <div className='footerDetails'>
+                <p className='creatorDetails'>Author: Alex Brown</p>
+                <p className='creatorDetails'>Updated: 10th Jan 2023</p>
+            </div>
         </footer>
     )
 }
